4612c4404b536fb2c983376d67466c6b330d60ef
Victoria Sawchuk <vtsawchuk@gmail.com>
2018-01-02 10:48:08 -0800

    Add changeLogInStatus function onPress of header button

--- src/ProfileScreen.js
+++ src/ProfileScreen.js
@@ -11,7 +11,18 @@ class SearchScreen extends Component {
       <Icon name="md-person" style={[{ color: tintColor }]} />
     ),
   }
+  constructor() {
+    super();
+    this.state = { loggedIn: false };
+    this.changeLogInStatus = this.changeLogInStatus.bind(this);
+  }
+  changeLogInStatus() {
+    this.setState((previousState) => {
+      return { loggedIn: !previousState.loggedIn };
+    });
+  }
   render() {
+    const headerButtonText = this.state.loggedIn ? 'Log Out' : 'Log In';
     return (
       <Container>
         <Header style={styles.header}>
@@ -20,8 +31,8 @@ class SearchScreen extends Component {
             <Title style={styles.greyText}>Profile</Title>
           </Body>
           <Right>
-            <Button transparent>
-              <Text style={styles.greyText}>Log Out</Text>
+            <Button transparent onPress={this.changeLogInStatus}>
+              <Text style={styles.greyText}>{ headerButtonText }</Text>
             </Button>
           </Right>
         </Header>
