41fe19faa49883482a687f40f2c5daa029340b3f
Victoria Sawchuk <vtsawchuk@gmail.com>
2018-01-01 21:34:12 -0800

    Begin styling profile screen header

--- src/ProfileScreen.js
+++ src/ProfileScreen.js
@@ -3,13 +3,13 @@ import {
   View,
   Text,
   Image,
-  TouchableHighlight,
+  Button,
 } from 'react-native';
 import styles from './styles.js';
 
 class SearchScreen extends Component {
   static navigationOptions = {
-    tabBarIcon: ({ tintColor }) => (
+    tabBarIcon: () => (
       <Image
         source={require('../images/profile-icon.png')}
         style={styles.navIcon}
@@ -19,8 +19,19 @@ class SearchScreen extends Component {
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View style={styles.profile}>
-        <Text>Profile Screen</Text>
+      <View>
+        <View style={styles.profileHeader}>
+          <View style={styles.flexContainer}/>
+          <View style={styles.flexContainer}>
+            <Text style={styles.headerTitle}>Profile</Text>
+          </View>
+          <View style={styles.flexContainer}>
+            <Button style={styles.profileHeaderButton} title="Log Out"/>
+          </View>
+        </View>
+        <View style={styles.profile}>
+          <Text>Profile Screen</Text>
+        </View>
       </View>
     );
   }
--- src/styles.js
+++ src/styles.js
@@ -2,6 +2,10 @@ import {
   StyleSheet,
 } from 'react-native';
 
+const GOLD = '#F9A602';
+const GREY = '#F3F2E9';
+const DARKBLUE = '#2F3553';
+
 const styles = StyleSheet.create({
   search: {
     backgroundColor: 'blue',
@@ -15,9 +19,29 @@ const styles = StyleSheet.create({
     backgroundColor: 'green',
     flex: 1,
   },
+  profileHeader: {
+    flexDirection: 'row',
+    height: 100,
+    backgroundColor: GOLD,
+  },
+  profileHeaderButton: {
+    fontSize: 15,
+  },
+  headerTitle: {
+    fontSize: 20,
+    color: GREY,
+  },
   navIcon: {
     height: 30,
     width: 30,
+  },
+  flexContainer: {
+    alignItems: 'flex-end',
+    flex: 1,
+    flexDirection: 'row',
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
   },
 });
 
