5f04d7a23696eea2550450b2cb93bfc8a70d0329
Victoria Sawchuk <vtsawchuk@gmail.com>
2018-01-04 14:26:26 -0800

    Resolve an eslint error

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
 import AppWithNavigationState from './src/navigators/AppNavigator';
 
-const App = () => {
-  return (
-    <Provider store={createStore(reducers)}>
-      <AppWithNavigationState />
-    </Provider>
-  );
-};
+const App = () => (
+  <Provider store={createStore(reducers)}>
+    <AppWithNavigationState />
+  </Provider>
+);
 
 export default App;
