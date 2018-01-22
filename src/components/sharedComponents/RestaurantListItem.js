44aa4a1585efe83f4f571aab0cf9a37f930a8796
Victoria Sawchuk <vtsawchuk@gmail.com>
2018-01-22 13:39:51 -0800

    Prevent restaurant list item text from overflowing onto buttons

--- src/components/sharedComponents/RestaurantListItem.js
+++ src/components/sharedComponents/RestaurantListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Linking, Image } from 'react-native';
+import { View, Text, Linking, Image, Dimensions } from 'react-native';
 import { Icon, ListItem, Body, Thumbnail, Button } from 'native-base';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
@@ -10,6 +10,7 @@ import FormattedAddress from './FormattedAddress';
 const RestaurantListItem = (props) => {
   const { restaurant, hasAddButton, setupAddRestaurant, source } = props;
   const { name, price, url } = restaurant;
+  const { screenHeight, screenWidth } = Dimensions.get('window');
   let imageUrl;
   let displayAddress;
   let yelpOffsetText;
@@ -32,27 +33,25 @@ const RestaurantListItem = (props) => {
     );
   }
   return (
-    <ListItem style={chowOffsetStyle}>
+    <ListItem style={[chowOffsetStyle, {width: screenWidth}]}>
       <Thumbnail square large source={{ uri: imageUrl }} />
-      <Body>
-        <View flexDirection="row" justifyContent="space-between" style={{ paddingLeft: 10 }}>
-          <View flexDirection="column" justifyContent="space-around">
-            <Text style={styles.restaurantName}>{name}</Text>
-            <YelpRating restaurant={restaurant} source={source} />
-            <FormattedAddress addressArray={displayAddress} />
-          </View>
-          <View flexDirection="column" justifyContent="space-around" alignItems="center" >
-            <Text>{price}</Text>
-            {addButton}
-            <Button
-              transparent
-              onPress={() => Linking.openURL(url)}
-              style={styles.yelpBurst}
-            >
-              <Text>{yelpOffsetText}</Text>
-              <Image source={require('../../../assets/yelp_burst/Screen/Yelp_burst_positive_RGB.png')} style={styles.yelpBurst} />
-            </Button>
-          </View>
+      <Body flexDirection="row" justifyContent="space-between" style={{ paddingLeft: 10 }}>
+        <View flex={5} flexDirection="column" justifyContent="space-around">
+          <Text style={styles.restaurantName}>{name}</Text>
+          <YelpRating restaurant={restaurant} source={source} />
+          <FormattedAddress addressArray={displayAddress} />
+        </View>
+        <View flexDirection="column" justifyContent="space-around" alignItems="center" >
+          <Text>{price}</Text>
+          {addButton}
+          <Button
+            transparent
+            onPress={() => Linking.openURL(url)}
+            style={styles.yelpBurst}
+          >
+            <Text>{yelpOffsetText}</Text>
+            <Image source={require('../../../assets/yelp_burst/Screen/Yelp_burst_positive_RGB.png')} style={styles.yelpBurst} />
+          </Button>
         </View>
       </Body>
     </ListItem>
