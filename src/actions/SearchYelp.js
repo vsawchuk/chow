4474611be177c84e76b7f158a54f5dd1e695ce3a
Victoria Sawchuk <vtsawchuk@gmail.com>
2018-01-23 09:57:36 -0800

    Remove comments on search yelp function

--- src/actions/SearchYelp.js
+++ src/actions/SearchYelp.js
@@ -4,25 +4,25 @@ import { loadSearchResults } from './LoadSearchResults';
 import { incrementUserSearchCount } from './IncrementUserSearchCount';
 
 export const searchYelp = (searchTerm, searchLocation, userLocation) => {
-  // let requestURL;
-  // if (searchLocation.length > 0) {
-  //   requestURL = `https://api.yelp.com/v3/businesses/search?term=${searchTerm}&location=${searchLocation}&limit=50`;
-  // } else {
-  //   requestURL = `https://api.yelp.com/v3/businesses/search?term=${searchTerm}&latitude=${userLocation.latitude}&longitude=${userLocation.longitude}&limit=50`;
-  // }
-  // const config = {
-  //   headers: { Authorization: `Bearer ${YELP_API_KEY}` },
-  // };
+  let requestURL;
+  if (searchLocation.length > 0) {
+    requestURL = `https://api.yelp.com/v3/businesses/search?term=${searchTerm}&location=${searchLocation}&limit=50`;
+  } else {
+    requestURL = `https://api.yelp.com/v3/businesses/search?term=${searchTerm}&latitude=${userLocation.latitude}&longitude=${userLocation.longitude}&limit=50`;
+  }
+  const config = {
+    headers: { Authorization: `Bearer ${YELP_API_KEY}` },
+  };
   return (dispatch) => {
     dispatch(incrementUserSearchCount());
-  //   axios.get(requestURL, config)
-  //     .then((response) => {
-  //       console.log(response.data.businesses[0]);
-  //       dispatch(loadSearchResults(response.data.businesses));
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  //
+    axios.get(requestURL, config)
+      .then((response) => {
+        console.log(response.data.businesses[0]);
+        dispatch(loadSearchResults(response.data.businesses));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
   }
 };
